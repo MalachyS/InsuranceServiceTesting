50ecbb0bb1dde101d9158c5a29c05101708c644c
S00221188 <S00221188@LAB-D2036-18>
2023-04-18 11:52:39 +0100

    Add project files.

---
+++ UnitTesting.Test/InsuranceService.cs
@@ -0,0 +1,43 @@
+﻿namespace UnitTesting
+{
+    public class InsuranceService
+    {
+        public double CalcPremium(int age, string location)
+        {
+            double premium = 0.0;
+            double discount = 0.5;
+
+            if (location == "rural")
+            {
+                if (age >= 18 && age < 30)
+                {
+                    premium = 5.0;
+                }
+                else if (age >= 30)
+                {
+                    premium = 2.50;
+                }
+            }
+            else if (location == "urban")
+            {
+                if (age >= 18 && age <= 35)
+                {
+                    premium = 6.0;
+                }
+                else if (age >= 36)
+                {
+                    premium = 5.0;
+                }
+            }
+
+
+
+             if (age >= 50)
+             {
+                 premium *= discount;
+             } 
+
+            return premium;
+        }
+    }
+}
---
+++ UnitTesting/InsuranceServiceTests.cs
@@ -0,0 +1,92 @@
+﻿using NUnit.Framework;
+using UnitTesting;
+
+namespace UnitTesting2.Tests
+{
+    [TestFixture]
+    public class InsuranceServiceTests
+    {
+        [TestFixture]
+        public class InsuranceServiceTests
+        {
+
+            [Test]
+            public void CalcPremium_Location_Rural_Between18_32_True()
+            {
+                // Arrange
+                InsuranceService service = new InsuranceService();
+
+                // Act
+                double premium = service.CalcPremium(25, "Rural");
+
+                // Assert
+                Assert.AreEqual(5, premium);
+            }
+
+            [Test]
+            public void CalcPremium_Location_Rural_over32_True()
+            {
+                // Arrange
+                InsuranceService service = new InsuranceService();
+
+                // Act
+                double premium = service.CalcPremium(33, "Rural");
+
+                // Assert
+                Assert.AreEqual(2.5, premium);
+            }
+
+            [Test]
+            public void CalcPremium_Location_Urban_over36_True()
+            {
+                // Arrange
+                InsuranceService service = new InsuranceService();
+
+                // Act
+                double premium = service.CalcPremium(37, "Urban");
+
+                // Assert
+                Assert.AreEqual(6, premium);
+            }  
+
+            [Test]
+            public void CalcPremium_Location_Urban_Between18_32_True()
+            {
+                // Arrange
+                InsuranceService service = new InsuranceService();
+
+                // Act
+                double premium = service.CalcPremium(25, "Urban");
+
+                // Assert
+                Assert.AreEqual(5, premium);
+            }
+            
+            [Test]
+            public void CalcPremium_50andUp_Rural()
+            {
+                // Arrange
+                InsuranceService service = new InsuranceService();
+
+                // Act
+                double premium = service.CalcPremium(50, "Rural");
+
+                // Assert
+                Assert.AreEqual(5, premium );
+            } 
+            
+            [Test]
+            public void CalcPremium_50andUp_Urban()
+            {
+                // Arrange
+                InsuranceService service = new InsuranceService();
+
+                // Act
+                double premium = service.CalcPremium(50, "Rural");
+
+                // Assert
+                Assert.AreEqual(5, premium );
+            }
+        }
+    }
+}
